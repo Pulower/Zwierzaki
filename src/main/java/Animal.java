d9e5cfd6fbe6b7190d30401efaa1576ab2919cb9
Pulower <w.dzik111@gmail.com>
2018-01-10 20:07:56 +0100

    Maven project

---
+++ src/main/java/Animal.java
@@ -0,0 +1,16 @@
+public abstract class Animal {
+
+    protected String name;
+    protected int howMuchEatPerDay;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public int eat(int amountOfFood) {
+        return amountOfFood - howMuchEatPerDay;
+    }
+
+    public void getSound() {
+    }
+}
